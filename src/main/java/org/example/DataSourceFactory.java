afde18eea92fb6bebf096ff8f9570bedb8c389f8
piotr.duzniak <piotr.duzniak@tis.biz>
2024-03-13 23:58:13 +0100

    Added logging

--- src/main/java/org/example/DataSourceFactory.java
+++ src/main/java/org/example/DataSourceFactory.java
@@ -3,17 +3,30 @@ package org.example;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 
 public class DataSourceFactory {
-
-
-        public static HikariDataSource create(String url, String username, String password) {
-            HikariConfig config = new HikariConfig();
-            config.setJdbcUrl( url );
-            config.setUsername( username );
-            config.setPassword( password );
-            config.addDataSourceProperty("maximumPoolSize", "1");
-            return new HikariDataSource( config );
+    static {
+        InputStream resourceAsStream = Main.class.getResourceAsStream("/logging.properties");
+        try {
+            LogManager.getLogManager().readConfiguration(resourceAsStream);
+        } catch (final IOException e) {
+            Logger.getAnonymousLogger().severe("Could not load default logging.properties file");
+            Logger.getAnonymousLogger().severe(e.getMessage());
         }
+    }
+
+    public static HikariDataSource create(String url, String username, String password) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.addDataSourceProperty("maximumPoolSize", "1");
+        return new HikariDataSource(config);
+    }
 
 }
